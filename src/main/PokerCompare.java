5b0b89a0b66c713542c27e2cfbda3309e43543fa
xjhxcc <2351889342@qq.com>
2019-08-11 14:08:48 +0800

    Fefactor:completet test case 3D4H_and_5H6D

--- src/main/PokerCompare.java
+++ src/main/PokerCompare.java
@@ -1,7 +1,12 @@
 package main;
 
+import java.util.List;
+
 public class PokerCompare {
     public static Poker comparePoker(Poker poker1, Poker poker2) {
         return poker1.getNum() >= poker2.getNum() ? poker1 : poker2;
+    }
+    public static Poker comparePoker(List<Poker> list1, List<Poker> list2) {
+        return null;
     }
 }
--- src/main/PokerCompareTest.java
+++ src/main/PokerCompareTest.java
@@ -2,9 +2,14 @@ package main;
 
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PokerCompareTest {
     @Test
-    public void should_return_2win_given_AD_and_KD() {
+    public void should_return_poker2_given_AD_and_KD() {
         //given
         Poker poker1 = new Poker('2', 'D');
         Poker poker2 = new Poker('3', 'D');
@@ -14,4 +19,20 @@ public class PokerCompareTest {
         Assert.assertEquals(poker2,result);
 
     }
+    @Test
+    public void should_return_poker1_given_3D4H_and_5H6D() {
+        //given
+        Poker poker1 = new Poker('3', 'D');
+        Poker poker2 = new Poker('4', 'H');
+        Poker poker3 = new Poker('5', 'H');
+        Poker poker4 = new Poker('6', 'D');
+        List<Poker> list1=Arrays.asList(poker1,poker2);
+        List<Poker> list2=Arrays.asList(poker3,poker4);
+        //when
+        Poker result = PokerCompare.comparePoker(list1,list2);
+        //then
+        Assert.assertEquals(poker2,result);
+
+    }
+
 }
